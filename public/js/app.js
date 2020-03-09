8dbd09423ea61fe81bca23e257458a4516bf1f60
GayaChoi <woosung827@naver.com>
2020-03-10 01:28:18 +0900

    레이아웃 다시 잡기

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,9 @@ let ul = document.getElementsByTagName("ul")[0],
 for (let index = 0; index < li.length; index++) {
     if (url.includes(li[index].children[0].pathname)) {
       if (li[0].children[0].classList.contains("active")) {
-        console.log(index);
         li[0].children[0].classList.remove("active");     
       }
       li[index].children[0].classList.add("active");
     }    
 }
+
