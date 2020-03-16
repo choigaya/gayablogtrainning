b97bfe6a4e0272d610bc686c3129dcb445a91912
GayaChoi <woosung827@naver.com>
2020-03-16 23:37:23 +0900

    아카이브 추가

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,24 @@
-let ul = document.getElementsByTagName("ul")[2],
-  li = ul.children;
-  url = location.pathname;
-  
-  console.log(ul);
-for (let index = 0; index < li.length; index++) {
-    if (url.includes(li[index].children[0].pathname)) {
-      if (li[0].children[0].classList.contains("active")) {
-        li[0].children[0].classList.remove("active");     
+let headerNavUl = document.getElementsByTagName("ul")[0],
+    headerNavLi = headerNavUl.children,
+    categoryUl = document.getElementsByTagName("ul")[2],
+    categoryLi = categoryUl.children,
+    url = location.pathname;
+     
+for (let i = 0; i < headerNavLi.length; i++) {
+    if (url.includes(headerNavLi[i].children[0].pathname)) {
+      if (headerNavLi[0].children[0].classList.contains("active")) {
+        headerNavLi[0].children[0].classList.remove("active");     
       }
-      li[index].children[0].classList.add("active");
+      headerNavLi[i].children[0].classList.add("active");
     }    
+}
+
+for (let j = 0; j < categoryLi.length; j++) {
+  if (url.includes(categoryLi[j].children[0].pathname)) {
+    if (categoryLi[0].children[0].classList.contains("active")) {
+      categoryLi[0].children[0].classList.remove("active");     
+    }
+    categoryLi[j].children[0].classList.add("active");
+  }    
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,84 +3,100 @@ const url = require("url");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log(req.url);
-  res.render("index", { 
-     title: "Gaya Blog",
-    });
+  res.render("index", {
+    title: "Gaya Blog"
+  });
 });
 
-router.get("/portfolio", (req, res, next) => {
+router.get("/categories/portfolio", (req, res, next) => {
   res.render("thumbnails", {
     title: "Gaya - 포트폴리오",
     heading: "포트폴리오",
-    pathname: "portfolio", 
+    pathname: req.url,
     number: 0,
     content:
       "테스트중입니다...테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다...."
   });
 });
 
-router.get("/develop", (req, res, next) => {
+router.get("/categories/develop", (req, res, next) => {
   res.render("thumbnails", {
     title: "Gaya - 개발공부",
     heading: "개발공부",
-    pathname: "develop", 
+    pathname: req.url,
     number: 0,
     content:
       "테스트중입니다...테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다...."
   });
 });
 
-router.get("/art", (req, res, next) => {
+router.get("/categories/art", (req, res, next) => {
   res.render("thumbnails", {
     title: "Gaya - 문화예술",
     heading: "문화예술",
-    pathname: "art",
+    pathname: req.url,
     number: 0,
     content:
       "테스트중입니다...테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다...."
   });
 });
 
-router.get("/account", (req, res, next) => {
+router.get("/categories/account", (req, res, next) => {
   res.render("thumbnails", {
     title: "Gaya - 재무/회계",
     heading: "재무/회계",
-    pathname: "account",
+    pathname: req.url,
     number: 0,
     content:
       "테스트중입니다...테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다...."
   });
 });
 
-router.get("/language", (req, res, next) => {
+router.get("/categories/language", (req, res, next) => {
   res.render("thumbnails", {
     title: "Gaya - 외국어공부",
     heading: "외국어공부",
-    pathname: "language",
+    pathname: req.url,
     number: 0,
     content:
       "테스트중입니다...테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다....테스트중입니다...."
   });
 });
 
-router.get("/blog", (req, res, next) => {
-  res.render("blog", { 
-    title: "Gaya - 블로그", 
+router.get("/categories/blog", (req, res, next) => {
+  res.render("blog", {
+    title: "Gaya - 블로그"
+  });
+});
+
+router.get("/archives", (req, res, next) => {
+  res.render("about", {
+    title: "Gaya - About"
+  });
+});
+
+router.get("/categories", (req, res, next) => {
+  res.render("about", {
+    title: "Gaya - About"
+  });
+});
+
+router.get("/tags", (req, res, next) => {
+  res.render("about", {
+    title: "Gaya - About"
   });
 });
 
 router.get("/about", (req, res, next) => {
-  res.render("about", { 
-    title: "Gaya - About",
-   });
+  res.render("about", {
+    title: "Gaya - About"
+  });
 });
 
-router.get("/:id/page/:id", (req, res, next) => {
-  /* console.log(url.parse(req.url).pathname); */
+router.get("/categories/:id/page/:id", (req, res, next) => {
   res.render("page", {
-     title: "Gaya - page" 
-    });
+    title: "Gaya - page"
+  });
 });
 
 router.get("*", (req, res, next) => {
